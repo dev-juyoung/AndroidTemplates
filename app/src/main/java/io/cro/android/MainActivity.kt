1fafa654a5b36f21c566a0d5b783f7a495b52da0
cro <dev.juyoung@gmail.com>
2019-09-20 00:40:20 +0900

    [ISSUE#1] Change support-library to androidx artifacts

--- app/src/main/java/io/cro/android/MainActivity.kt
+++ app/src/main/java/io/cro/android/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.cro.android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
