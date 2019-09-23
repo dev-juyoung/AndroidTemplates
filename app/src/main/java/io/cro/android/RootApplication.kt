d7358adc8c14c3f8382a44ee896a196f59153a58
Juyoung, Lee <dev.juyoung@gmail.com>
2019-09-23 11:16:54 +0900

    [ISSUE#3] Added logger library initialize blocks

--- app/src/main/java/io/cro/android/RootApplication.kt
+++ app/src/main/java/io/cro/android/RootApplication.kt
@@ -3,9 +3,22 @@ package io.cro.android
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import io.cro.android.di.DaggerApplicationComponent
+import io.cro.android.extensions.isDebuggable
+import timber.log.Timber
 
 class RootApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.factory().create(applicationContext)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        setupDevTools()
+    }
+
+    private fun setupDevTools() {
+        if (!isDebuggable) return
+
+        Timber.plant(Timber.DebugTree())
     }
 }
